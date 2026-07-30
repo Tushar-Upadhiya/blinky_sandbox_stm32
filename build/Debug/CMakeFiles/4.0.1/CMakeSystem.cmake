d9eff037c11467175a619920b865815d956caa26
robocop72-encrypted <tusharupadhiya2@gmail.com>
2026-07-30 13:03:15 +0530

    Reset project template for current STM32 board

---
+++ build/Debug/CMakeFiles/4.0.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.18.13-arch1-1")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.18.13-arch1-1")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/Tushar/experiment_and_learning_sandbox/stm32_store_room/blinky_sandbox/cmake/gcc-arm-none-eabi.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
